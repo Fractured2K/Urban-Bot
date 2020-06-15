6d531d6b265f5b569f28e5308238a738636b3cae
Fractured2K <32372095+Fractured2K@users.noreply.github.com>
2020-06-14 17:44:28 -0700

    :zap: Implement caching for definitions and wotd

--- cogs/urban.py
+++ cogs/urban.py
@@ -2,11 +2,14 @@ import discord
 from discord.ext import commands
 from scrapers import define_word, scrape_wotd
 from utils.messages import error_message, define_message
+from expiringdict import ExpiringDict
 
 
 class Commands(commands.Cog):
     def __init__(self, client):
         self.client = client
+        # 3600 seconds (1 hour)
+        self.cache = ExpiringDict(max_len=100, max_age_seconds=3600)
 
     @commands.command()
     async def help(self, ctx):
@@ -41,34 +44,48 @@ class Commands(commands.Cog):
         word = ''
         result = 0
 
+        # Parse word and result to return
         for arg in args:
             if arg.isnumeric():
                 result = int(arg)
             else:
                 word += f" {arg}"
 
-        # Scrape Word
-        word = define_word(word, result)
+        # check if the word is cached
+        word_definition = self.cache.get(f"{word}-{result}")
 
-        if "error" in word:
-            embed = error_message(word["error"])
+        if not word_definition:
+            # scrape Word
+            word_definition = define_word(word, result)
+
+            # cache word
+            self.cache[f"{word}-{result}"] = word_definition
+
+        if "error" in word_definition:
+            embed = error_message(word_definition["error"])
             return await ctx.send(embed=embed)
         else:
-            embed = define_message(word)
+            embed = define_message(word_definition)
             return await ctx.send(embed=embed)
 
     @commands.command()
     async def wotd(self, ctx):
-        # Check if redis store contains current wotd
+        # Check if wotd is cached
+        wotd = self.cache.get('wotd')
 
-        # Scrape current wotd
-        wotd = scrape_wotd()
+        if not wotd:
+            # scrape current wotd
+            wotd = scrape_wotd()
 
-        # Store wotd
+            # cache wotd
+            self.cache["wotd"] = wotd
 
         # Return wotd
         embed = define_message(wotd)
         return await ctx.send(embed=embed)
+
+    # @commands.command()
+    # async def trending(self, ctx):
 
 
 def setup(client):
