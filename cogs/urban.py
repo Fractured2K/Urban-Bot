6c22940e49cad46dd7a0cf2cad6287de88482e43
Fractured2K <32372095+Fractured2K@users.noreply.github.com>
2020-06-16 00:40:20 -0700

    :sparkles: Create trending command

--- cogs/urban.py
+++ cogs/urban.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-from scrapers import define_word, scrape_wotd
-from utils.messages import error_message, define_message
+from scrapers import scrape_word, scrape_wotd, scrape_trending_words, scrape_random_word
+from utils.messages import error_message, define_message, trending_message
 from expiringdict import ExpiringDict
 
 
@@ -25,38 +25,38 @@ class Commands(commands.Cog):
             name="define", value="Accepts two arguments `!string {word}` and `?int {result}`. e.g `.define stupendous`", inline=False)
         embed.add_field(
             name="wotd", value="Returns the word of the day and its definition", inline=False)
+        embed.add_field(
+            name="trending", value="Returns a list of today's currently trending words. If a number is passed, the word correlated with that number will be defined. e.g `.trending 2`")
 
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def define(self, ctx):
-        args = ctx.message.content.split(' ')[1:]
+    async def define(self, ctx, word='', result=0):
+        if not word:
+            # Parse message arguments if word not provided
+            args = ctx.message.content.split(' ')[1:]
 
-        # Invalid argument length
-        if len(args) == 0:
-            return await ctx.send("Define accepts two arguments `!string {word}` and `?int {result}`")
+            # Invalid argument length
+            if len(args) == 0:
+                return await ctx.send("Define accepts two arguments `!string {word}` and `?int {result}`")
 
-        # Invalid argument order
-        if args[0].isnumeric():
-            return await ctx.send("Define requires at least one argument `{word}`")
+            # Invalid argument order
+            if args[0].isnumeric():
+                return await ctx.send("Define requires at least one argument `{word}`")
 
-        # Store
-        word = ''
-        result = 0
-
-        # Parse word and result to return
-        for arg in args:
-            if arg.isnumeric():
-                result = int(arg)
-            else:
-                word += f" {arg}"
+            # Parse word and result to return
+            for arg in args:
+                if arg.isnumeric():
+                    result = int(arg)
+                else:
+                    word += f" {arg}"
 
         # check if the word is cached
         word_definition = self.cache.get(f"{word}-{result}")
 
         if not word_definition:
             # scrape Word
-            word_definition = define_word(word, result)
+            word_definition = scrape_word(word, result)
 
             # cache word
             self.cache[f"{word}-{result}"] = word_definition
@@ -84,8 +84,33 @@ class Commands(commands.Cog):
         embed = define_message(wotd)
         return await ctx.send(embed=embed)
 
-    # @commands.command()
-    # async def trending(self, ctx):
+    @commands.command()
+    async def trending(self, ctx):
+        # Parse args
+        args = ctx.message.content.split(' ')[1:]
+
+        # Check if trending data is cached
+        trending = self.cache.get("trending-cmd")
+
+        if not trending:
+            # Scrape trending words
+            trending = scrape_trending_words()
+
+            # Cache trending words
+            self.cache["trending-cmd"] = trending
+
+        if len(args) != 0 and args[0].isnumeric():
+            num = int(args[0])
+
+            if num > len(trending):
+                return await ctx.send(f"Please choose a number between 1 and {len(trending)}")
+
+            word = trending[num-1]["word"]
+
+            await self.define(ctx, word)
+        else:
+            embed = trending_message(trending)
+            await ctx.send(embed=embed)
 
 
 def setup(client):
--- utils/messages.py
+++ utils/messages.py
@@ -11,3 +11,19 @@ def define_message(word):
                   description=word["word_definition"], color=0x0b4ee7)
 
     return embed
+
+
+def trending_message(trending_words):
+    embed = Embed(title="Trending Words", description="A list of today's currently trending words.", color=0x0b4ee7,
+                  url="https://www.urbandictionary.com/")
+
+    for word_dict in trending_words:
+        word = word_dict["word"]
+        num = word_dict["num"]
+
+        embed.add_field(name="\u200b", value=f"{num}. {word}", inline=True)
+
+    embed.set_footer(
+        text="Type .help for to view all possible commands")
+
+    return embed
