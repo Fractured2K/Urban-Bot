982c0db2d28cc11ae17c17f37c4a4f22839afe4f
Fractured2K <32372095+Fractured2K@users.noreply.github.com>
2020-06-25 05:43:14 -0700

    :sparkles: Enrich definition and defintion message
    Added more fields to definitions enriching user experience

--- utils/messages.py
+++ utils/messages.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from discord import Embed
 
 
@@ -22,10 +23,28 @@ def error_message(error):
     return Embed(title=error, color=0xf55353)
 
 
-def define_message(word):
-    embed = Embed(title=word["word"],
-                  description=word["word_definition"], color=0x0b4ee7)
+def define_message(definition_block):
+    # Word, Description, Word URL, Embed Color
+    embed = Embed(title=definition_block["word"],
+                  description=definition_block["word_definition"], url=definition_block["url"], color=0x0b4ee7)
 
+    # Embed Thumbnail
+    fallback_avatar = "https://cdn.discordapp.com/app-icons/720363121386193009/a686399461394ed4912e78175d962ebc.png"
+    embed.set_thumbnail(url=(os.getenv("AVATAR_URL") or fallback_avatar))
+
+    # Add field for each example
+    for num, example in enumerate(definition_block["word_examples"]):
+        embed.add_field(name=f"Example #{num+1}", value=example, inline=False)
+
+    # Word ratings
+    ratings = definition_block["ratings"]
+    like_emoji = "<:thumbsup:725685268413415484>"
+    dislike_emoji = "<:thumbsdown:725686415240986714>"
+    embed.add_field(name="Likes/Dislikes",
+                    value=f"{like_emoji} {ratings[0]} {dislike_emoji} {ratings[1]}")
+
+    # Word contributor (Author)
+    embed.set_footer(text=definition_block["contributor"])
     return embed
 
 
--- utils/parsers.py
+++ utils/parsers.py
@@ -1,8 +1,7 @@
 def parse_definitions(soup):
     """
-    Parses definitions from soup, 
-    returning a list of dictionaries that contain 
-    a word, word_definition and example of the word.
+    Parses and formats definitions from soup, 
+    returning a list of dictionaries.
 
     :param soup:
     :return definitions:
@@ -20,6 +19,11 @@ def parse_definitions(soup):
         # Defined word -> "Stupendous"
         word = definition.find(class_="word").text
 
+        # Word URL
+        word_url = word.split(' ')
+        word_url = '+'.join(word_url)
+        word_url = f"https://www.urbandictionary.com/define.php?term{word_url}"
+
         # Word definition -> "Of colossal excellence or greatness. The most esteemed level of hierarchical stature."
         word_definition = definition.find(class_="meaning").text
 
@@ -29,12 +33,20 @@ def parse_definitions(soup):
         cleaned_word_examples = [
             word for word in word_examples if len(word) != 0]
 
-        # Format scraped definition
+        # Contributor -> "by Evolved guppy June 22, 2020"
+        word_contributor = definition.find(class_="contributor").text
+
+        # Definition Likes/Dislikes
+        word_ratings = definition.find_all(class_="count")
+        word_ratings = [word_ratings[0].text, word_ratings[1].text]
+
         formatted_definition = dict()
         formatted_definition["word"] = word
+        formatted_definition["url"] = word_url
         formatted_definition["word_definition"] = word_definition
-        formatted_definition.setdefault(
-            "word_examples", []).append(cleaned_word_examples)
+        formatted_definition["contributor"] = word_contributor
+        formatted_definition["word_examples"] = cleaned_word_examples
+        formatted_definition["ratings"] = word_ratings
 
         definitions.append(formatted_definition)
 
